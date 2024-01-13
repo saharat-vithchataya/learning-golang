8eb722a72460a05a3007661eb6e285a27a82307d
Saharat Vithchataya <vithchataya.saharat@gmail.com>
2024-01-14 00:38:16 +0700

    hexagonal architecture 1 [update some code]

--- banking/main.go
+++ banking/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
@@ -16,6 +17,20 @@ import (
 
 func main() {
 	initConfig()
+	db := initDatabase()
+	// customerRepositoryMock := repository.NewCustomerRepositoryMock()
+	customerRepository := repository.NewCustomerRepositoryDB(db)
+	customerService := services.NewCustomerService(customerRepository)
+	customerHandler := handler.NewCustomerHandler(customerService)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/customers", customerHandler.GetCustomers)
+	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer)
+	log.Printf("Banking service started at port %v", viper.GetInt("app.port"))
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
+}
+
+func initDatabase() *sqlx.DB {
 	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
 		viper.GetString("db.host"),
 		viper.GetInt("db.port"),
@@ -27,17 +42,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	_ = db
-	// customerRepository := repository.NewCustomerRepositoryDB(db)
-	customerRepositoryMock := repository.NewCustomerRepositoryMock()
-	customerService := services.NewCustomerService(customerRepositoryMock)
-	customerHandler := handler.NewCustomerHandler(customerService)
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return db
 
-	router := mux.NewRouter()
-	router.HandleFunc("/customers", customerHandler.GetCustomers)
-	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer)
-	log.Printf("Banking service started at port %v", viper.GetInt("app.port"))
-	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
 }
 
 func initConfig() {
